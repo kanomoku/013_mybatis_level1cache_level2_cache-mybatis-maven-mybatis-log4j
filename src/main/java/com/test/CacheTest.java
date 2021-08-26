cd968e043579ea956a61fd5152ae871f71a55c8d
kanomoku <1049505015@qq.com>
2021-08-26 19:59:45 +0800

    add

--- src/main/java/com/test/CacheTest.java
+++ src/main/java/com/test/CacheTest.java
@@ -8,16 +8,18 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
 
 import com.pojo.LogInfo;
 
-public class Test {
-	public static void main(String[] args) throws IOException {
+public class CacheTest {
+//	public static void main(String[] args) throws IOException {
 
-		// 构建者设计模式
+	@Test
+	public void test1() throws IOException {
+
 		InputStream inputStream = Resources.getResourceAsStream("mybatis.xml");
 		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
-
 
 //		1. 应用程序和数据库交互的过程是一个相对比较耗时的过程
 //		2. 缓存存在的意义:让应用程序减少对数据库的访问,提升程序运行效率
@@ -35,6 +37,9 @@ public class Test {
 		for (LogInfo logInfo : selectList1) {
 //			System.out.println(logInfo);
 		}
+		SqlSession session1 = factory.openSession();
+		List<LogInfo> selectList11 = session1.selectList("com.mapper.LogMapper.selAll1");
+
 		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 //		4. 缓存流程
 //			4.1 步骤一: 先去缓存区中找是否存在statement
@@ -42,17 +47,20 @@ public class Test {
 //			4.3 步骤三:如果没有缓存statement 对象,去数据库获取数据
 //			4.4 步骤四:数据库返回查询结果
 //			4.5 步骤五:把查询结果放到对应的缓存区中
-		List<LogInfo> selectList2 = session.selectList("com.mapper.LogMapper.selAll2");//两次查询只有一个sql
+		List<LogInfo> selectList2 = session.selectList("com.mapper.LogMapper.selAll2");// 两次查询只有一个sql
 		for (LogInfo logInfo : selectList2) {
 //			System.out.println(logInfo);
 		}
-		List<LogInfo> selectList3 = session.selectList("com.mapper.LogMapper.selAll2");//两次查询只有一个sql
+		List<LogInfo> selectList3 = session.selectList("com.mapper.LogMapper.selAll2");// 两次查询只有一个sql
 		for (LogInfo logInfo : selectList3) {
 //			System.out.println(logInfo);
 		}
 		session.close();
 		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+	}
 
+	@Test
+	public void test2() throws IOException {
 //		5. SqlSessionFactory 缓存
 //			5.1 又叫:二级缓存
 //			5.2 有效范围:同一个factory 内哪个SqlSession 都可以获取
@@ -62,27 +70,31 @@ public class Test {
 //				5.4.1 在mapper.xml 中添加<cache readOnly="true"></cache>
 //				5.4.2 如果不写readOnly=”true”需要把实体类序列化
 //			5.5 当SqlSession 对象close()时或commit()时会把SqlSession 缓存的数据刷(flush)到SqlSessionFactory 缓存区中
+
+		InputStream inputStream = Resources.getResourceAsStream("mybatis.xml");
+		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
 		
-/*		SqlSession session2 = factory.openSession();
+		SqlSession session2 = factory.openSession();
 		List<LogInfo> session2selectList2 = session2.selectList("com.mapper.LogMapper.selAll1");
 		for (LogInfo object : session2selectList2) {
-//			System.out.println(object);
+			// System.out.println(object); }
 		}
+
 		session2.close();// 注意这里不关闭 以及缓存不会往二级缓存里面存
 		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		
 		SqlSession session3 = factory.openSession();
 		List<LogInfo> session3selectList3 = session3.selectList("com.mapper.LogMapper.selAll1");
 		for (LogInfo object : session3selectList3) {
-//			System.out.println(object);
+			// System.out.println(object); } session3.close();
 		}
-		session3.close();
 		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
 		SqlSession session4 = factory.openSession();
 		List<LogInfo> session4selectList4 = session4.selectList("com.mapper.LogMapper.selAll2");
 		for (LogInfo object : session4selectList4) {
-//			System.out.println(object);
+			// System.out.println(object); } session4.close();
 		}
-		session4.close();*/
 	}
 
 }
